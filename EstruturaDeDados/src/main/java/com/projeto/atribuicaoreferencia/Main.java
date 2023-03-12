4222cbeadf6078f4c69ef59caf86320e5194f129
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-03-12 19:46:52 -0400

    Atribuição e referência em java:Tipos primitivos e objetos

--- EstruturaDeDados/src/main/java/com/projeto/atribuicaoreferencia/Main.java
+++ EstruturaDeDados/src/main/java/com/projeto/atribuicaoreferencia/Main.java
@@ -2,6 +2,18 @@ package com.projeto.atribuicaoreferencia;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int  nA = 1;
+        int nB = nA;
+        System.out.println("nA=" + nA + "nB=" + nB);
+
+        nA = 2;
+        System.out.println("nA=" + nA + "nB=" + nB);
+
+        MyObject objectA = new MyObject(1);
+        MyObject objectB = objectA;
+        System.out.println("objectA=" + objectA + "objectB=" + objectB);
+        objectA.setNum(2);
+        System.out.println("objectA=" + objectA + "objectB=" + objectB);
+
     }
 }
